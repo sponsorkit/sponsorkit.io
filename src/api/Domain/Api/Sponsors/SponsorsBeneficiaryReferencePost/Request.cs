956aeb8e6c2d77719a92b5d14194ea344ccd2ea6
Mathias Lorenzen <mathias.lorenzen@live.com>
2021-04-28 20:17:14 +0200

    separate request.

---
+++ src/api/Domain/Api/Sponsors/SponsorsBeneficiaryReferencePost/Request.cs
@@ -0,0 +1,9 @@
+﻿namespace Sponsorkit.Domain.Api.Sponsors.SponsorsBeneficiaryReferencePost
+{
+    public class Request
+    {
+        public int? AmountInHundreds { get; set; }
+        public string? Email { get; set; }
+        public string? StripeCardId { get; set; }
+    }
+}
