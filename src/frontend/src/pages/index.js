6e337b1c735beb87b0e9e364ed7e2cf9ec423ffe
Mathias Lykkegaard Lorenzen <mathias.lorenzen@live.com>
2021-05-24 08:10:38 +0200

    Create index.js

---
+++ src/frontend/src/pages/index.js
@@ -0,0 +1,19 @@
+import * as React from "react"
+import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
+
+import Layout from "../components/layout"
+
+const IndexPage = () => (
+  <Layout>
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <p>
+      <Link to="/page-2/">Go to page 2</Link> <br />
+      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    </p>
+  </Layout>
+)
+
+export default IndexPage
