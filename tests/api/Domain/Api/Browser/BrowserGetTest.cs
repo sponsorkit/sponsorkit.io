e6aa48d3ffbf952fdf34febb7515f0b9a165e1f4
Mathias Lorenzen <mathias.lorenzen@live.com>
2021-05-25 19:19:57 +0200

    added dummy test.

---
+++ tests/api/Domain/Api/Browser/BrowserGetTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Sponsorkit.Tests.Domain.Api.Browser
+{
+    [TestClass]
+    public class BrowserGetTest
+    {
+        [TestMethod]
+        public async Task BrowserGet_NoParametersGiven_CanFetchFunction()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
