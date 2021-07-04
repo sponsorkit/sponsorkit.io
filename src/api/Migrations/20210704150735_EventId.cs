96013649828894ba4d31b7f00c1fd85ea4e49619
Mathias Lorenzen <mathias.lorenzen@live.com>
2021-07-04 17:15:00 +0200

    added migration.

---
+++ src/api/Migrations/20210704150735_EventId.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Sponsorkit.Migrations
+{
+    public partial class EventId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "StripeEventId",
+                table: "Bounties",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StripeEventId",
+                table: "Bounties");
+        }
+    }
+}
