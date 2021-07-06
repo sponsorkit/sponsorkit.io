a3826e30c52058d6d2457b2c44a05bab295ef7c3
Mathias Lorenzen <mathias.lorenzen@live.com>
2021-07-06 22:56:28 +0200

    fix for tooltips.

--- src/frontend/src/pages/dashboard/index.tsx
+++ src/frontend/src/pages/dashboard/index.tsx
@@ -1,5 +1,5 @@
 import CircularProgressBar from "@components/circular-progress-bar";
-import { Box, Button, Card, CardContent, Container, Paper, Typography } from "@material-ui/core";
+import { Box, Button, Card, CardContent, Container, Paper, Tooltip, Typography } from "@material-ui/core";
 import { CheckBox, CheckBoxOutlineBlank, QuestionAnswer } from "@material-ui/icons";
 import { AppBarTemplate } from "..";
 import PrivateRoute from "../../components/login/private-route";
@@ -27,15 +27,19 @@ function DashboardPage() {
                 checkpoints={[
                     {
                         label: "Connect your GitHub account",
-                        validate: () => true
+                        description: "Connecting with GitHub allows you to receive donations and sponsorships.",
+                        validate: () => true,
+                        onClick: () => { }
                     },
                     {
                         label: "Verify e-mail address",
+                        description: "Verifying your e-mail address allows you to receive an e-mail whenever you earn money.",
                         validate: () => account.isEmailVerified,
                         onClick: () => { }
                     },
                     {
                         label: "Complete Stripe profile",
+                        description: "Filling in your bank account and payout details with Stripe allows you to withdraw earned money to your bank account.",
                         validate: () => account.isEmailVerified,
                         onClick: () => { }
                     }
@@ -47,15 +51,18 @@ function DashboardPage() {
                 checkpoints={[
                     {
                         label: "Connect your GitHub account",
+                        description: "Connecting with GitHub allows us to know which GitHub user should be associated with bounties you create.",
                         validate: () => true
                     },
                     {
                         label: "Verify e-mail address",
+                        description: "Verifying your e-mail address allows you to receive an e-mail whenever your card has been charged.",
                         validate: () => account.isEmailVerified,
                         onClick: () => { }
                     },
                     {
                         label: "Save payment details for later",
+                        description: "Payment information is stored with Stripe. Saving it makes it easier for you to create bounties, donations or sponsor someone in the future.",
                         validate: () => !!account.sponsor?.creditCard,
                         onClick: () => { }
                     }
@@ -71,6 +78,7 @@ function AccountOverview(props: {
     checkpoints: Array<{
         validate: () => boolean,
         label: string,
+        description: string,
         onClick?: () => void
     }>
 }) {
@@ -95,17 +103,19 @@ function AccountOverview(props: {
                 <Box>
                     <ul>
                         {props.checkpoints.map(x =>
-                            <li className={x.validate() ? 
-                                classes.complete : 
-                                classes.incomplete}
-                            >
-                                {x.validate() ?
-                                    <CheckBox color="primary" /> :
-                                    <CheckBoxOutlineBlank color="disabled" />}
-                                <Typography className={classes.typography}>
-                                    {x.label}
-                                </Typography>
-                            </li>)}
+                            <Tooltip title={x.description}>
+                                <li className={x.validate() ? 
+                                    classes.complete : 
+                                    classes.incomplete}
+                                >
+                                    {x.validate() ?
+                                        <CheckBox color="primary" /> :
+                                        <CheckBoxOutlineBlank color="disabled" />}
+                                    <Typography className={classes.typography}>
+                                        {x.label}
+                                    </Typography>
+                                </li>
+                            </Tooltip>)}
                     </ul>
                 </Box>
             </Box>
