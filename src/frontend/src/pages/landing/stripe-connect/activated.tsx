25e246c13ae22bd0fe4a2a57609ab3e4162622d0
Mathias Lorenzen <mathias.lorenzen@live.com>
2021-10-02 21:05:51 +0000

    fix for activated landing page.

---
+++ src/frontend/src/pages/landing/stripe-connect/activated.tsx
@@ -0,0 +1,8 @@
+import LandingPage from "@components/landing-page";
+
+export default () => {
+    return <LandingPage 
+        logoVariant="sponsorkit"
+        title="Your bank account details have been saved!"
+        continueUrl="/dashboard" />
+}
