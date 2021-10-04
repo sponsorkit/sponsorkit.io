24346a8b2a3c1c258ad6d522fa6bfbcc40d5370f
Mathias Lorenzen <mathias.lorenzen@live.com>
2021-10-04 08:19:56 +0200

    yesterday's progress committed/pushed.

---
+++ tests/api/Domain/Mediatr/EnsureGitHubIssueInDatabaseCommandTest.cs
@@ -0,0 +1,86 @@
+﻿using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Sponsorkit.Tests.Domain.Mediatr
+{
+    [TestClass]
+    public class EnsureGitHubIssueInDatabaseCommandTest
+    {
+        [TestMethod]
+        public async Task Handle_NoRepositoryFound_ReturnsNotFound()
+        {
+            //Arrange
+            
+            //Act
+            
+            //Assert
+            Assert.Fail("Not implemented.");
+        }
+        
+        [TestMethod]
+        public async Task Handle_NoIssueFound_ReturnsNotFound()
+        {
+            //Arrange
+            
+            //Act
+            
+            //Assert
+            Assert.Fail("Not implemented.");
+        }
+        
+        [TestMethod]
+        public async Task Handle_NoDatabaseRepositoryFound_CreatesNewRepository()
+        {
+            //Arrange
+            
+            //Act
+            
+            //Assert
+            Assert.Fail("Not implemented.");
+        }
+        
+        [TestMethod]
+        public async Task Handle_ExistingDatabaseRepositoryFound_UsesExistingRepository()
+        {
+            //Arrange
+            
+            //Act
+            
+            //Assert
+            Assert.Fail("Not implemented.");
+        }
+        
+        [TestMethod]
+        public async Task Handle_NoDatabaseIssueFound_CreatesNewIssue()
+        {
+            //Arrange
+            
+            //Act
+            
+            //Assert
+            Assert.Fail("Not implemented.");
+        }
+        
+        [TestMethod]
+        public async Task Handle_ExistingDatabaseIssueFound_ReturnsExistingIssue()
+        {
+            //Arrange
+            
+            //Act
+            
+            //Assert
+            Assert.Fail("Not implemented.");
+        }
+        
+        [TestMethod]
+        public async Task Handle_IssueHappensDuringIssueCreation_RepositoryChangesAreRolledBack()
+        {
+            //Arrange
+            
+            //Act
+            
+            //Assert
+            Assert.Fail("Not implemented.");
+        }
+    }
+}
---
+++ tests/api/Domain/Mediatr/UpsertIssueCommentCommandTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Sponsorkit.Tests.Domain.Mediatr
+{
+    [TestClass]
+    public class UpsertIssueCommentCommandTest
+    {
+        [TestMethod]
+        public async Task Handle_ExistingBotCommentFound_UpdatesExistingComment()
+        {
+            //Arrange
+            
+            //Act
+            
+            //Assert
+            Assert.Fail("Not implemented.");
+        }
+        
+        [TestMethod]
+        public async Task Handle_NoExistingBotCommentFound_CreatesNewComment()
+        {
+            //Arrange
+            
+            //Act
+            
+            //Assert
+            Assert.Fail("Not implemented.");
+        }
+    }
+}
