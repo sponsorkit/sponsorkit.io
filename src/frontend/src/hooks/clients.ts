9dc2356d486b805cc3aa73639f87e0fe053035bb
Mathias Lorenzen <mathias.lorenzen@live.com>
2021-10-12 18:09:55 +0000

    update

--- src/frontend/src/hooks/clients.ts
+++ src/frontend/src/hooks/clients.ts
@@ -42,7 +42,7 @@ function getBaseUri() {
     if (currentUri.hostname === "localhost")
         return "http://localhost:5000/";
 
-    if(currentUri.hostname.endsWith(".gatsbyjs.io"))
+    if(currentUri.hostname.endsWith(".gtsb.io"))
         return "https://api-staging.sponsorkit.io/";
 
     const requestUri = new URL(window.location.href);
