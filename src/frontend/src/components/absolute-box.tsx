3a54796ba2cdec8d7a4ed52c5f824475dc5949ae
Mathias Lorenzen <mathias.lorenzen@live.com>
2021-10-15 09:58:56 +0000

    fixed #75.

--- src/frontend/src/components/absolute-box.tsx
+++ src/frontend/src/components/absolute-box.tsx
@@ -35,7 +35,7 @@ export default function AbsoluteBox(props: {
                 if(isNaN(rectHeight))
                     return;
         
-                if(rectHeight !== height) {
+                if(rectHeight !== height && rectHeight > 0) {
                     setHeight(rectHeight);
                 }
             }
@@ -44,6 +44,11 @@ export default function AbsoluteBox(props: {
             onUpdate();
 
             observer.observe(absoluteBoxDom);
+
+            return () => {
+                console.info("disconnect-observer");
+                observer.disconnect();
+            };
         },
         [ref, ref.current]);
 
