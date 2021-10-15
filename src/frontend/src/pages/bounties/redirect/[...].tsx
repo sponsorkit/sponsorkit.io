5432fdfa12c6b955ea1f499cc2cace79c2f30e71
Mathias Lorenzen <mathias.lorenzen@live.com>
2021-10-15 11:20:41 +0000

    tweak to redirect.

--- src/frontend/src/pages/bounties/redirect/[...].tsx
+++ src/frontend/src/pages/bounties/redirect/[...].tsx
@@ -3,19 +3,23 @@ import { extractIssueLinkDetails, getBountyhuntUrlFromIssueLinkDetails } from "@
 export default function RedirectPage(props: {
     location: Location
 }) {
-    if(!props.location)
+    if(!props.location?.href)
         return null;
 
-    const url = new URL(props.location.href);
-    url.pathname = url.pathname.replace(/^\/bounties\/redirect\//, '');
-    url.host = "github.com";
+    try {
+        const url = new URL(props.location.href);
+        url.pathname = url.pathname.replace(/^\/bounties\/redirect\//, '');
+        url.host = "github.com";
 
-    const metadata = extractIssueLinkDetails(url.href);
-    if(!metadata)
-        return null;
+        const metadata = extractIssueLinkDetails(url.href);
+        if(!metadata)
+            return null;
 
-    const redirectUrl = getBountyhuntUrlFromIssueLinkDetails(metadata);
-    window.location.href = redirectUrl;
+        const redirectUrl = getBountyhuntUrlFromIssueLinkDetails(metadata);
+        window.location.href = redirectUrl;
+    } catch(e) {
+        console.error(e);
+    }
 
     return null;
 }
