584fed2970487b9527fb03a1478ac93ffde221ff
Mathias Lorenzen <mathias.lorenzen@live.com>
2023-01-16 20:13:11 +0100

    Remove unused usings.

--- tests/api/Reflection/EmailTemplateTest.cs
+++ tests/api/Reflection/EmailTemplateTest.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
-using Amazon.SimpleEmailV2.Model;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Sponsorkit.Domain.Mediatr.Email;
 
