946d309dbf40f619a8f584768e5d13e1a0355785
Mathias Lorenzen <mathias.lorenzen@live.com>
2023-02-12 15:02:47 +0100

    Fix test.

--- tests/api/Domain/Api/Webhooks/Stripe/StripeWebhookPostTest.cs
+++ tests/api/Domain/Api/Webhooks/Stripe/StripeWebhookPostTest.cs
@@ -1,13 +1,17 @@
-﻿using System.Threading.Tasks;
+﻿using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Primitives;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NSubstitute;
+using NSubstitute.ExceptionExtensions;
 using Sponsorkit.Domain.Controllers.Webhooks.Stripe;
 using Sponsorkit.Domain.Controllers.Webhooks.Stripe.Handlers;
 using Sponsorkit.Tests.TestHelpers;
 using Sponsorkit.Tests.TestHelpers.Environments.Sponsorkit;
+using Stripe;
 
 namespace Sponsorkit.Tests.Domain.Api.Webhooks.Stripe;
 
@@ -36,8 +40,23 @@ public class StripeWebhookPostTest
     {
         //Arrange
         var fakeHandler1 = Substitute.For<IStripeEventHandler>();
-        var fakeHandler2 = Substitute.For<IStripeEventHandler>();
+        fakeHandler1
+            .CanHandleWebhookType(Arg.Any<string>(), Arg.Any<object>())
+            .Returns(true);
         
+        var fakeHandler2 = Substitute.For<IStripeEventHandler>();
+        fakeHandler2
+            .CanHandleWebhookType(Arg.Any<string>(), Arg.Any<object>())
+            .Returns(true);
+
+        var fakeEventFactory = Substitute.For<IEventFactory>();
+        fakeEventFactory
+            .CreateEvent(Arg.Any<string>(), Arg.Any<StringValues>())
+            .Returns(new Event()
+            {
+                Data = new EventData()
+            });
+
         await using var environment = await SponsorkitIntegrationTestEnvironment.CreateAsync(new ()
         {
             IocConfiguration = services =>
@@ -46,6 +65,8 @@ public class StripeWebhookPostTest
                 
                 services.AddSingleton(fakeHandler1);
                 services.AddSingleton(fakeHandler2);
+
+                services.AddSingleton(fakeEventFactory);
             }
         });
 
@@ -76,32 +97,122 @@ public class StripeWebhookPostTest
     public async Task HandleAsync_MultipleElligibleHandlersFound_ReturnsOk()
     {
         //Arrange
+        var fakeHandler1 = Substitute.For<IStripeEventHandler>();
+        fakeHandler1
+            .CanHandleWebhookType(Arg.Any<string>(), Arg.Any<object>())
+            .Returns(true);
+        
+        var fakeHandler2 = Substitute.For<IStripeEventHandler>();
+        fakeHandler2
+            .CanHandleWebhookType(Arg.Any<string>(), Arg.Any<object>())
+            .Returns(true);
+
+        var fakeEventFactory = Substitute.For<IEventFactory>();
+        fakeEventFactory
+            .CreateEvent(Arg.Any<string>(), Arg.Any<StringValues>())
+            .Returns(new Event()
+            {
+                Data = new EventData()
+            });
+
+        await using var environment = await SponsorkitIntegrationTestEnvironment.CreateAsync(new ()
+        {
+            IocConfiguration = services =>
+            {
+                services.RemoveAll<IStripeEventHandler>();
+                
+                services.AddSingleton(fakeHandler1);
+                services.AddSingleton(fakeHandler2);
+
+                services.AddSingleton(fakeEventFactory);
+            }
+        });
+
+        var handler = environment.ServiceProvider.GetRequiredService<StripeWebhookPost>();
+        handler.EnsureControllerContext();
+        handler.Request.Headers.Add("Stripe-Signature", "some-signature");
             
         //Act
+        var result = await handler.HandleAsync();
             
         //Assert
-        Assert.Fail("Not implemented.");
+        Assert.IsInstanceOfType<OkResult>(result);
     }
         
     [TestMethod]
     public async Task HandleAsync_NoHandlersFound_ReturnsOk()
     {
         //Arrange
+        var fakeEventFactory = Substitute.For<IEventFactory>();
+        fakeEventFactory
+            .CreateEvent(Arg.Any<string>(), Arg.Any<StringValues>())
+            .Returns(new Event()
+            {
+                Data = new EventData()
+            });
+
+        await using var environment = await SponsorkitIntegrationTestEnvironment.CreateAsync(new ()
+        {
+            IocConfiguration = services =>
+            {
+                services.RemoveAll<IStripeEventHandler>();
+
+                services.AddSingleton(fakeEventFactory);
+            }
+        });
+
+        var handler = environment.ServiceProvider.GetRequiredService<StripeWebhookPost>();
+        handler.EnsureControllerContext();
+        handler.Request.Headers.Add("Stripe-Signature", "some-signature");
             
         //Act
+        var result = await handler.HandleAsync();
             
         //Assert
-        Assert.Fail("Not implemented.");
+        Assert.IsInstanceOfType<OkResult>(result);
     }
         
     [TestMethod]
     public async Task HandleAsync_EventAlreadyHandled_ReturnsOk()
     {
         //Arrange
+        var fakeHandler = Substitute.For<IStripeEventHandler>();
+        fakeHandler
+            .CanHandleWebhookType(Arg.Any<string>(), Arg.Any<object>())
+            .Returns(true);
+
+        fakeHandler
+            .HandleAsync(Arg.Any<string>(), Arg.Any<object>(), Arg.Any<CancellationToken>())
+            .Throws(new EventAlreadyHandledException());
+        
+        var fakeEventFactory = Substitute.For<IEventFactory>();
+        fakeEventFactory
+            .CreateEvent(Arg.Any<string>(), Arg.Any<StringValues>())
+            .Returns(new Event()
+            {
+                Data = new EventData()
+            });
+
+        await using var environment = await SponsorkitIntegrationTestEnvironment.CreateAsync(new ()
+        {
+            IocConfiguration = services =>
+            {
+                services.RemoveAll<IStripeEventHandler>();
+                
+                services.AddSingleton(fakeHandler);
+
+                services.AddSingleton(fakeEventFactory);
+            }
+        });
+
+        var handler = environment.ServiceProvider.GetRequiredService<StripeWebhookPost>();
+        handler.EnsureControllerContext();
+        handler.Request.Headers.Add("Stripe-Signature", "some-signature");
             
         //Act
+        var result = await handler.HandleAsync();
             
         //Assert
-        Assert.Fail("Not implemented.");
+        Assert.IsTrue(result is OkObjectResult { Value: "Already handled." });
     }
 }
