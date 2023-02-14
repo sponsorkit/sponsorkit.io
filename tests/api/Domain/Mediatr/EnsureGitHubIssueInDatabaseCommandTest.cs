a8130058d60ac69064d2b04dc133e69407a372a8
Mathias Lorenzen <mathias.lorenzen@live.com>
2023-02-14 19:01:31 +0100

    Fix for more tests.

--- tests/api/Domain/Mediatr/EnsureGitHubIssueInDatabaseCommandTest.cs
+++ tests/api/Domain/Mediatr/EnsureGitHubIssueInDatabaseCommandTest.cs
@@ -1,5 +1,14 @@
-﻿using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using NSubstitute.Extensions;
+using Octokit;
+using Octokit.GraphQL.Core;
+using Sponsorkit.Domain.Mediatr;
+using Sponsorkit.Tests.TestHelpers.Environments.Sponsorkit;
+using Sponsorkit.Tests.TestHelpers.Octokit;
 
 namespace Sponsorkit.Tests.Domain.Mediatr;
 
@@ -10,33 +19,73 @@ public class EnsureGitHubIssueInDatabaseCommandTest
     public async Task Handle_NoRepositoryFound_ReturnsNotFound()
     {
         //Arrange
-            
+        await using var environment = await SponsorkitIntegrationTestEnvironment.CreateAsync();
+
         //Act
+        var result = await environment.Mediator.Send(
+            new EnsureGitHubIssueInDatabaseCommand(
+                "owner",
+                "name",
+                1));
             
         //Assert
-        Assert.Fail("Not implemented.");
+        Assert.IsFalse(result.IsSuccess);
+        Assert.AreEqual("Repository not found.", result.Errors.Single());
     }
         
     [TestMethod]
     public async Task Handle_NoIssueFound_ReturnsNotFound()
     {
         //Arrange
-            
+        await using var environment = await SponsorkitIntegrationTestEnvironment.CreateAsync();
+
+        environment.GitHub.FakeClient.Repository
+            .Get(Arg.Any<string>(), Arg.Any<string>())
+            .Returns(new TestRepository());
+        
         //Act
-            
+        var result = await environment.Mediator.Send(
+            new EnsureGitHubIssueInDatabaseCommand(
+                "owner",
+                "name",
+                1337));
+
         //Assert
-        Assert.Fail("Not implemented.");
+        Assert.IsFalse(result.IsSuccess);
+        Assert.AreEqual("Issue not found.", result.Errors.Single());
     }
         
     [TestMethod]
     public async Task Handle_NoDatabaseRepositoryFound_CreatesNewRepository()
     {
         //Arrange
-            
+        await using var environment = await SponsorkitIntegrationTestEnvironment.CreateAsync();
+
+        var gitHubIssue = new TestIssue();
+        environment.GitHub.FakeClient.Issue
+            .Get(Arg.Any<string>(), Arg.Any<string>(), Arg.Any<int>())
+            .Returns(gitHubIssue);
+        
+        var gitHubRepository = new TestRepository();
+        environment.GitHub.FakeClient.Repository
+            .Get(Arg.Any<string>(), Arg.Any<string>())
+            .Returns(gitHubRepository);
+        
         //Act
-            
+        var result = await environment.Mediator.Send(
+            new EnsureGitHubIssueInDatabaseCommand(
+                gitHubRepository.Owner.Login,
+                gitHubRepository.Name,
+                gitHubIssue.Number));
+
         //Assert
-        Assert.Fail("Not implemented.");
+        var databaseRepository = await environment.Database.WithoutCachingAsync(async dataContext =>
+            await dataContext.Repositories.SingleOrDefaultAsync());
+        Assert.IsNotNull(databaseRepository);
+        
+        Assert.AreEqual(gitHubRepository.Id, databaseRepository.GitHub.Id);
+        Assert.AreEqual(gitHubRepository.Name, databaseRepository.GitHub.Name);
+        Assert.AreEqual(gitHubRepository.Owner.Login, databaseRepository.GitHub.OwnerName);
     }
         
     [TestMethod]
@@ -54,11 +103,33 @@ public class EnsureGitHubIssueInDatabaseCommandTest
     public async Task Handle_NoDatabaseIssueFound_CreatesNewIssue()
     {
         //Arrange
-            
+        await using var environment = await SponsorkitIntegrationTestEnvironment.CreateAsync();
+
+        var gitHubIssue = new TestIssue();
+        environment.GitHub.FakeClient.Issue
+            .Get(Arg.Any<string>(), Arg.Any<string>(), Arg.Any<int>())
+            .Returns(gitHubIssue);
+        
+        var gitHubRepository = new TestRepository();
+        environment.GitHub.FakeClient.Repository
+            .Get(Arg.Any<string>(), Arg.Any<string>())
+            .Returns(gitHubRepository);
+        
         //Act
-            
+        var result = await environment.Mediator.Send(
+            new EnsureGitHubIssueInDatabaseCommand(
+                gitHubRepository.Owner.Login,
+                gitHubRepository.Name,
+                gitHubIssue.Number));
+
         //Assert
-        Assert.Fail("Not implemented.");
+        var databaseIssue = await environment.Database.WithoutCachingAsync(async dataContext =>
+            await dataContext.Issues.SingleOrDefaultAsync());
+        Assert.IsNotNull(databaseIssue);
+        
+        Assert.AreEqual(gitHubIssue.Id, databaseIssue.GitHub.Id);
+        Assert.AreEqual(gitHubIssue.Number, databaseIssue.GitHub.Number);
+        Assert.AreEqual(gitHubIssue.Title, databaseIssue.GitHub.TitleSnapshot);
     }
         
     [TestMethod]
