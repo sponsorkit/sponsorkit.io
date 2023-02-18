f176def2c111ff4a8ab5fefddb89cc9807749518
Mathias Lorenzen <mathias.lorenzen@live.com>
2023-02-18 13:15:57 +0100

    Added renaming.

---
+++ src/backend/Sponsorkit.BusinessLogic/Migrations/20230218121521_AddedCompletedDate.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sponsorkit.Migrations
+{
+    public partial class AddedCompletedDate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ExpiredAt",
+                table: "BountyClaimRequests",
+                newName: "CompletedAt");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CompletedAt",
+                table: "BountyClaimRequests",
+                newName: "ExpiredAt");
+        }
+    }
+}
---
+++ tests/api/Jobs/PayoutJobTest.cs
@@ -0,0 +1,69 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using Sponsorkit.Api.Domain.Controllers.Api.Bounties.SetupIntent;
+using Sponsorkit.Tests.TestHelpers;
+using Sponsorkit.Tests.TestHelpers.Environments.Sponsorkit;
+using Sponsorkit.Tests.TestHelpers.Octokit;
+using Stripe;
+
+namespace Sponsorkit.Tests.Jobs;
+
+[TestClass]
+public class PayoutJobTest
+{
+
+    [TestMethod]
+    public async Task FullFlow_TODO()
+    {
+        //Arrange
+        await using var environment = await SponsorkitIntegrationTestEnvironment.CreateAsync();
+
+        environment.GitHub.FakeClient.Repository
+            .Get(
+                "repository-owner",
+                "repository-name")
+            .Returns(new TestRepository());
+
+        environment.GitHub.FakeClient.Issue
+            .Get(
+                "repository-owner",
+                "repository-name",
+                1337)
+            .Returns(new TestIssue());
+
+        var authenticatedUser = await environment.Database.UserBuilder
+            .WithStripeCustomer(environment.Stripe.CustomerBuilder
+                .WithDefaultPaymentMethod(environment.Stripe.PaymentMethodBuilder))
+            .BuildAsync();
+
+        var setupIntentPost = environment.ServiceProvider.GetRequiredService<SetupIntentPost>();
+        setupIntentPost.FakeAuthentication(authenticatedUser);
+        
+        var preconditionBounty = await environment.Database.WithoutCachingAsync(async context =>
+            await context.Bounties.SingleOrDefaultAsync());
+        Assert.IsNull(preconditionBounty);
+        
+        var result = await setupIntentPost.HandleAsync(new PostRequest(
+            new GitHubIssueRequest(
+                "repository-owner",
+                "repository-name",
+                1337),
+            10_00));
+        var response = result.ToResponseObject();
+        
+        var refreshedIntent = await environment.Stripe.SetupIntentService.ConfirmAsync(response.PaymentIntent.Id);
+        Assert.AreEqual("succeeded", refreshedIntent.Status);
+        
+        await environment.Stripe.WaitForWebhookAsync(ev => ev.Type == Events.SetupIntentSucceeded);
+        
+        //Act
+        
+        //Assert
+
+        Assert.Fail();
+    }
+}
