45cb9bc11c80126c53b5c21299fe66ad9c5a73cc
Mathias Lorenzen <mathias.lorenzen@live.com>
2023-02-18 01:26:30 +0100

    Ready to deploy staging job.

--- src/backend/Sponsorkit.Jobs/Program.cs
+++ src/backend/Sponsorkit.Jobs/Program.cs
@@ -1,5 +1,20 @@
-using System.Runtime.CompilerServices;
+using System.Text.Json;
+using Amazon.Lambda.Core;
+using Amazon.Lambda.RuntimeSupport;
+using Amazon.Lambda.Serialization.SystemTextJson;
 
-[assembly: InternalsVisibleTo("Sponsorkit.Tests")]
+async Task Handler(JobRequest jobRequest, ILambdaContext context)
+{
+    await Task.Delay(1);
+    Console.WriteLine($"Executing job: {jobRequest.Job}");
+}
 
-throw new NotImplementedException();
+await LambdaBootstrapBuilder
+    .Create(
+        (Func<JobRequest, ILambdaContext, Task>)Handler, 
+        new DefaultLambdaJsonSerializer(options => 
+            options.PropertyNamingPolicy = JsonNamingPolicy.CamelCase))
+    .Build()
+    .RunAsync();
+
+record JobRequest(string Job);
