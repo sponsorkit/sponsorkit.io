2f7ee666a8665197f3f7d530ce4306cb7c93c559
ffMathy <mathias.lorenzen@live.com>
2024-01-14 14:55:50 +0100

    feat: add landing page for setup intents

---
+++ src/frontend/src/pages/landing/stripe-setup-intent/index.tsx
@@ -0,0 +1,7 @@
+import LandingPage from "@components/landing-page";
+
+export default () => {
+    return <LandingPage 
+        logoVariant="sponsorkit"
+        title="Your payment information was accepted!" />
+}
